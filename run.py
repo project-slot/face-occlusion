88c23754ef5697d6f51864651c5af68dde2f3275
TungVLe <tunle0801@gmail.com>
2023-11-25 14:14:32 -0500

    run on webcam

--- run.py
+++ run.py
@@ -6,17 +6,17 @@ from ultralytics import YOLO
 vid = cv2.VideoCapture(0) 
 model = YOLO("./runs/detect/train2/weights/best.pt")
   
-ret = False
+ret = True
 while(ret): 
       
     ret, frame = vid.read() 
 
-    result = model.predict(frame)
-    frame = result.plot()
+    result = model(frame)
+    frame = result[0].plot()
   
     cv2.imshow('frame', frame) 
       
-    if cv2.waitKey(1) & 0xFF == ord('q'): 
+    if cv2.waitKey(30) & 0xFF == ord('q'): 
         break
   
 vid.release() 
